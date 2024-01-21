cbe83286abc89df433e328ca77da2cac6150a50f
Mitchell Rivet <mitchellrivet@MitchelacStudio.lan>
2024-01-21 10:37:37 -0800

    add french and switch statement

--- go_with_tests/hello_world/hello_world.go
+++ go_with_tests/hello_world/hello_world.go
@@ -2,17 +2,35 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefix + name + "!"
+
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spanishPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
+
 }
 
 func main() {
 	// printing to stdout is a side effect
 	// this tutorial suggests separating side effects from pure functions
-	fmt.Println(Hello("Dave"))
+	fmt.Println(Hello("Dave", ""))
 }
--- go_with_tests/hello_world/hello_world_test.go
+++ go_with_tests/hello_world/hello_world_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
-		want := Hello("Mitch")
+		want := Hello("Mitch", "")
 		got := "Hello, Mitch!"
 
 		assertCorrectMessage(t, got, want)
@@ -12,11 +12,24 @@ func TestHelloWorld(t *testing.T) {
 
 	t.Run("Default to 'Hello, World!' when no name is supplied", func(t *testing.T) {
 
-		want := Hello("")
+		want := Hello("", "")
 		got := "Hello, World!"
 
 		assertCorrectMessage(t, got, want)
 
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		want := Hello("Elodie", "Spanish")
+		got := "Hola, Elodie!"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("In French", func(t *testing.T) {
+		want := Hello("Jean", "French")
+		got := "Bonjour, Jean!"
+		assertCorrectMessage(t, got, want)
 	})
 
 }
