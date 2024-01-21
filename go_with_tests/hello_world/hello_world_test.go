75ddc4cc2dbf4e2cf5d461ce828bc732f1ab0a3c
Mitchell Rivet <mitchellrivet@MitchelacStudio.lan>
2024-01-21 10:24:51 -0800

    expand hello_world test

--- go_with_tests/hello_world/hello_world_test.go
+++ go_with_tests/hello_world/hello_world_test.go
@@ -3,11 +3,28 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	want := Hello("Mitch")
-	got := "Hello, Mitch!"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		want := Hello("Mitch")
+		got := "Hello, Mitch!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Default to 'Hello, World!' when no name is supplied", func(t *testing.T) {
+
+		want := Hello("")
+		got := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got: %q, want: %q", got, want)
 	}
-
 }
